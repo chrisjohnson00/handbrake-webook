7cbe508125f90f71dc9f247ca998a56055ca6a5f
Chris Johnson <csjohnson3@wsgc.com>
2020-07-11 22:04:03 -0700

    Updating to publish the file to a kafka topic to avoid i/o overload

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
 import logging
 import os
+import consul
+from json import dumps
+from kafka import KafkaProducer, KafkaClient
 from api_client.client import get_full_file_path, get_quality_level
 
 application = Flask(__name__)
 application.logger.setLevel(logging.INFO)
-required_configs = []
+required_configs = ['KAFKA_TOPIC', 'KAFKA_SERVER']
+CONFIG_PATH = "handbrake-webhook"
 
 
 @application.route('/')
@@ -13,21 +17,33 @@ def index():
     return 'Welcome to the Handbrake Webhook Server!'
 
 
-@application.route('/webhook', methods=['GET', 'POST'])
+@application.route('/webhook', methods=['POST'])
 def web_hook():
+    producer = KafkaProducer(bootstrap_servers=[get_config('KAFKA_SERVER')],
+                             acks=1,
+                             value_serializer=lambda x:
+                             dumps(x).encode('utf-8'))
     application.logger.info("Web hook called")
     application.logger.info("Web hook headers: {}".format(request.headers))
     application.logger.info("Web hook data: {}".format(request.get_json()))
     application.logger.info("Calculated file path is {}".format(get_full_file_path(request.get_json())))
     application.logger.info("Calculated quality level is {}".format(get_quality_level(request.get_json())))
-    application.logger.info("Destination path is {}".format(get_move_path(get_quality_level(request.get_json()))))
-    copy_file(get_full_file_path(request.get_json()), get_move_path(get_quality_level(request.get_json())))
+    kafka_message = {'source_full_path': get_full_file_path(request.get_json()), 'move_type': 'to_encode',
+                     'type': 'tv', 'quality': get_quality_level(
+            request.get_json())}
+    application.logger.info("Sending message {} to topic '{}'".format(kafka_message, get_config("KAFKA_TOPIC")))
+    future = producer.send(topic=get_config("KAFKA_TOPIC"),
+                           value=kafka_message)
+    result = future.get(timeout=60)
+
     return 'Done'
 
 
 @application.route('/health')
 def health_check():
-    # put logic here to ensure we are happy to fulfill user requests
+    client = KafkaClient(bootstrap_servers=[get_config('KAFKA_SERVER')])
+    if not client.bootstrap_connected():
+        raise Exception("Unable to connect to Kafka: {}".format(get_config('KAFKA_SERVER')))
     return "Success"
 
 
@@ -36,7 +52,7 @@ def config():
     application.logger.info("Rendering config page")
     response_text = ""
     for config in required_configs:
-        value = application.config.get(config)
+        value = get_config(config)
         if any(secret in config for secret in ['KEY', 'TOKEN', 'PASSWORD']):
             response_text += "{}: [REDACTED]<br/>".format(config)
         else:
@@ -44,21 +60,18 @@ def config():
     return response_text
 
 
-def copy_file(src, dest):
-    command = ["cp", src, "{}/.".format(dest)]
-    application.logger.info("File copy command called {}".format(command))
-    # subprocess.run(command, check=True)
+# def copy_file(src, dest):
+#     command = ["cp", src, "{}/.".format(dest)]
+#     application.logger.info("File copy command called {}".format(command))
+#     # subprocess.run(command, check=True)
 
 
-def get_move_path(quality):
-    return get_config("watch{}".format(quality))
-
-
-def get_config(key):
+def get_config(key, config_path=CONFIG_PATH):
     if os.environ.get(key):
         return os.environ.get(key)
-    else:
-        raise ValueError("{} is not a valid quality level or isn't configured with a destination path yet".format(key))
+    c = consul.Consul()
+    index, data = c.kv.get("{}/{}".format(config_path, key))
+    return data['Value'].decode("utf-8")
 
 
 if __name__ == "__main__":
