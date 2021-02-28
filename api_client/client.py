7b115afb474ce9bf1a9a970b9ea333e259bc60ec
Chris Johnson <chrisjohnson00@hotmail.com>
2021-02-27 22:08:49 -0800

    feat: Adding radarr client

--- api_client/client.py
+++ api_client/client.py
@@ -1,26 +1,75 @@
-def get_full_file_path(api_json):
-    """
-    From the api json, grab the series.path and then append that to the episodeFile.relativePath to get the full path
-    :param api_json: a dict containing the json
-    :return: The full file path for the file from the json
-    """
-    base_path = api_json['series']['path']
-    relative_path = api_json['episodeFile']['relativePath']
-    return "{}/{}".format(base_path, relative_path)
+from abc import ABC, abstractmethod
 
 
-def get_quality_level(api_json):
-    """
-    From the api json, determine the quality by inspecting episodeFile.quality
-    :param api_json:
-    :return:
-    """
-    quality = api_json['episodeFile']['quality']
-    if "1080p" in quality:
-        return "1080p"
-    else:
-        return "720p"
+class ArrClient(ABC):
+
+    def __init__(self, api_json):
+        self.api_json = api_json
+
+    @abstractmethod
+    def get_full_file_path(self):
+        """
+        From the api json, grab the series.path and then append that to the episodeFile.relativePath to get the full
+        path
+        :return: The full file path for the file from the json
+        """
+        pass
+
+    @abstractmethod
+    def get_quality_level(self):
+        """
+        From the api json, determine the quality by inspecting episodeFile.quality
+        :return:
+        """
+        pass
+
+    def get_event_type(self):
+        return self.api_json['eventType']
 
 
-def get_event_type(api_json):
-    return api_json['eventType']
+class SonarrClient(ArrClient):
+
+    def get_full_file_path(self):
+        """
+        From the api json, grab the series.path and then append that to the episodeFile.relativePath to get the full
+        path
+        :return: The full file path for the file from the json
+        """
+        base_path = self.api_json['series']['path']
+        relative_path = self.api_json['episodeFile']['relativePath']
+        return "{}/{}".format(base_path, relative_path)
+
+    def get_quality_level(self):
+        """
+        From the api json, determine the quality by inspecting episodeFile.quality
+        :return:
+        """
+        quality = self.api_json['episodeFile']['quality']
+        if "1080p" in quality:
+            return "1080p"
+        else:
+            return "720p"
+
+
+class RadarrClient(ArrClient):
+
+    def get_full_file_path(self):
+        """
+        From the api json, grab the series.path and then append that to the episodeFile.relativePath to get the full
+        path
+        :return: The full file path for the file from the json
+        """
+        base_path = self.api_json['movie']['folderPath']
+        relative_path = self.api_json['movieFile']['relativePath']
+        return "{}/{}".format(base_path, relative_path)
+
+    def get_quality_level(self):
+        """
+        From the api json, determine the quality by inspecting episodeFile.quality
+        :return:
+        """
+        quality = self.api_json['movieFile']['quality']
+        if "1080p" in quality:
+            return "1080p"
+        else:
+            return "720p"
